97d5741e13910165517ed11150c3a5c7cd255ba7
confused-ace-noises <alt.alt.24.02@gmail.com>
2025-11-30 15:32:33 +0100

    removed old script

--- bees/script.sh
+++
@@ -1,8 +0,0 @@
-echo "===== FILE: Cargo.toml ====="
-cat Cargo.toml
-echo
-find src -type f -name "*.rs" | sort | while read -r file; do
-  echo "===== FILE: $file ====="
-  cat "$file"
-  echo -e "\n"
-done
