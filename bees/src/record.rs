d521787ebf8a64cb227f9b65d907fb662f6ed894
confused-ace-noises <alt.alt.24.02@gmail.com>
2026-02-17 08:50:14 +0100

    Revert "rehauled the whole project structure *again*. Too lazy to list all changes here, it should suffice to say that now a lot less stuff is `dyn` and that stuff is in turn statically determined at compile-time, like endpoint architecture"
    This reverts commit 9e90a31ca985905509140ee5128f193c6d943183.

--- bees/src/record.rs
+++
@@ -1,8 +0,0 @@
-use std::sync::Arc;
-
-use crate::capability::Capability;
-
-pub trait Record {
-    const SHARED_URL: &str;
-    fn shared_caps() -> Arc<[Box<dyn Capability>]>;
-}
--- bees/src/resources/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod resource_handler;
-pub mod resource;
-pub mod dyn_resource;
